2dc1ad16a6d21412655082fc2f2b89bc6bdfdb75
alexandraBouillon <alexandra.bouillon@gmail.com>
2021-03-15 18:16:53 -0700

    added readline functionality, started the  get move method

--- starter/humanPlayer.js
+++ starter/humanPlayer.js
@@ -1,8 +1,25 @@
+const readline = require('readline');
+
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const askQuestion = async () => {
+  try{
+    rl.question("Enter a coordinance",(answer) => {
+
+    })
+  } catch (err) {
+    console.error();
+  }
+}
+
+askQuestion()
+
 class HumanPlayer {
   constructor() {
     // TODO: Create a new readline interface and store is as an instance
     // variable (this.rl). Remember to require the built-in node module,
     // 'readline'
+    this.rl = readline.createInterface(process.stdin, process.stdout);
   }
 
   getMove() {
